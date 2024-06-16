01c2ecbbd843a920231a24d414823fb2e28632c2
Dan White <dan.mw1@hotmail.co.uk>
2024-06-16 14:35:56 +0100

    Bullets destroy enemies when hitting them

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,13 @@ fn main() {
         .add_systems(Startup, (spawn_player, spawn_camera))
         .add_systems(
             Update,
-            (player_movement, shoot, bullet_movement, spawn_enemies),
+            (
+                player_movement,
+                shoot,
+                bullet_movement,
+                spawn_enemies,
+                enemy_hit,
+            ),
         )
         .run();
 }
@@ -207,6 +213,31 @@ fn spawn_enemies(
             {
                 spawn_enemy_state_next_state.set(SpawnEnemyStatus::Ready)
             }
+        }
+    }
+}
+
+fn enemy_hit(
+    mut commands: Commands,
+    bullet_query: Query<(Entity, &Transform), With<Bullet>>,
+    enemy_query: Query<(Entity, &Transform), With<Enemy>>,
+) {
+    for (bullet_entity, bullet_transform) in bullet_query.iter() {
+        let mut bullet_destroyed = false;
+        for (enemy_entity, enemy_transform) in enemy_query.iter() {
+            if bullet_transform
+                .translation
+                .distance(enemy_transform.translation)
+                < ENEMY_SIZE / 2.0
+            {
+                commands.entity(bullet_entity).despawn_recursive();
+                commands.entity(enemy_entity).despawn_recursive();
+            }
+            bullet_destroyed = true;
+            break;
+        }
+        if bullet_destroyed {
+            break;
         }
     }
 }
