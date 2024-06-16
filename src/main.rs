78621781a9b8141b831841d884a365bd4065b6be
Dan White <dan.mw1@hotmail.co.uk>
2024-06-16 14:04:42 +0100

    Space shoots bullet

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,15 @@ use bevy::sprite::MaterialMesh2dBundle;
 use bevy::window::PrimaryWindow;
 
 const PLAYER_SPEED: f32 = 500.;
+const PLAYER_WIDTH: f32 = 40.;
+const PLAYER_HEIGHT: f32 = 60.;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .init_state::<ShootStatus>()
         .add_systems(Startup, (spawn_player, spawn_camera))
-        .add_systems(Update, (player_movement, shoot))
+        .add_systems(Update, (player_movement, shoot, bullet_movement))
         .run();
 }
 
@@ -83,18 +85,46 @@ enum ShootStatus {
     Fired(Instant),
 }
 
+#[derive(Component)]
+struct Bullet {
+    speed: f32,
+}
+
+const BULLET_SPEED: f32 = 650.;
+
 const SHOOT_COOLDOWN: u64 = 2000;
 
 fn shoot(
+    mut commands: Commands,
     shoot_state: Res<State<ShootStatus>>,
     mut shoot_state_next_state: ResMut<NextState<ShootStatus>>,
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut _player_query: Query<&mut Transform, With<Player>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    player_query: Query<&Transform, With<Player>>,
 ) {
     match **shoot_state {
         ShootStatus::Ready => {
             if keyboard_input.just_pressed(KeyCode::Space) {
                 dbg!("pew pew!");
+
+                let player_transform = player_query.get_single().unwrap();
+
+                commands.spawn((
+                    MaterialMesh2dBundle {
+                        mesh: meshes.add(Rectangle::new(5., 12.5)).into(),
+                        material: materials.add(ColorMaterial::from(Color::CRIMSON)),
+                        transform: Transform::from_translation(Vec3::new(
+                            player_transform.translation.x + PLAYER_WIDTH / 2.0,
+                            player_transform.translation.y + PLAYER_HEIGHT / 2.0,
+                            0.,
+                        )),
+                        ..default()
+                    },
+                    Bullet {
+                        speed: BULLET_SPEED,
+                    },
+                ));
                 shoot_state_next_state.set(ShootStatus::Fired(Instant::now()))
             }
         }
@@ -102,6 +132,21 @@ fn shoot(
             if Instant::now().duration_since(time_fired) > Duration::from_millis(SHOOT_COOLDOWN) {
                 shoot_state_next_state.set(ShootStatus::Ready)
             }
+        }
+    }
+}
+
+fn bullet_movement(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    mut bullet_query: Query<(Entity, &mut Transform, &Bullet), With<Bullet>>,
+    time: Res<Time>,
+) {
+    let window = window_query.get_single().unwrap();
+    for (entity, mut transform, bullet) in bullet_query.iter_mut() {
+        transform.translation += Vec3::Y * bullet.speed * time.delta_seconds();
+        if transform.translation.y > window.height() {
+            commands.entity(entity).despawn_recursive();
         }
     }
 }
